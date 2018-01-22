dc2412c7c240342ee4fc01a731ee19c4fde75a68
tseifert <tobias.seifert@trigonon.de>
2018-01-22 10:39:18 +0100

    docs added and code style changes

--- webtorrent-jquery.js
+++ webtorrent-jquery.js
@@ -1,31 +1,38 @@
-(function( $ ) {
-  $.fn.webtorrent = function( options ) {
-    if( typeof options === 'undefined' ) {
+(function ($) {
+  'use strict';
+  /**
+   * @param options.client      instance of WebTorrent
+   * @param options.torrentId   get passed to client.add(torrentId)
+   * @param options.opts        get passed to client.add(torrentId,opts)
+   * @param options.torrentOn   event-handlers that get assigned in client.add() through torrent.on()
+   */
+  $.fn.webtorrent = function (options) {
+    if (typeof options === 'undefined') {
       options = {};
     }
-    if( typeof options.client === 'undefined' ) {
+    if (typeof options.client === 'undefined') {
       options.client = new WebTorrent();
     }
-    if( typeof options.torrentId === 'undefined' ) {
-      options.torrentId = this.data( 'torrent-id' );
+    if (typeof options.torrentId === 'undefined') {
+      options.torrentId = this.data('torrent-id');
     }
-    if( typeof options.opts === 'undefined' ) {
+    if (typeof options.opts === 'undefined') {
       options.opts = {};
     }
-    if( typeof options.events === 'undefined' ) {
-      options.events = {};
+    if (typeof options.torrentOn === 'undefined') {
+      options.torrentOn = {};
     }
-    var $elems = this.find( '[data-filename]' );
-    options.client.add( options.torrentId, options.opts, function( torrent ) {
-      $.each( options.events, function( event, handler ) {
-        torrent.on( event, handler );
-      } );
-      $elems.each( function( key, elem ) {
-        torrent.files.find( function( file ) {
-          return file.name === $( elem ).data( 'filename' );
-        } ).renderTo( elem );
-      } );
-    } );
+    var $elems = this.find('[data-filename]');
+    options.client.add(options.torrentId, options.opts, function (torrent) {
+      $.each(options.torrentOn, function (event, handler) {
+        torrent.on(event, handler);
+      });
+      $elems.each(function (key, elem) {
+        torrent.files.find(function (file) {
+          return file.name === $(elem).data('filename');
+        }).renderTo(elem);
+      });
+    });
   };
-  $( '[data-toggle="webtorrent"]' ).webtorrent();
-}( jQuery ));
+  $('[data-toggle="webtorrent"]').webtorrent();
+}(jQuery));
